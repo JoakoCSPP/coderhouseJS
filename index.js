df17ce924d783bf3b4328e165d1fe5e9ffb82b35
Joako <joaquin2005cornejo@gmail.com>
2023-07-09 16:59:19 -0300

    pre entrega 2

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-alert("Ingresar cual producto desea, para salir ingresar 0 o ESC")
-let seleccionarProductos = Number(prompt("1-remera 2-shorts 3-camisa 4-pantalon 5-billetera 6-lentes"))
-let cantidadComprada;
+const persona = {
+    edad: prompt("Confirme su edad: ")
+}
+if (persona.edad < 18){
+    console.log("Eres menor de edad, ¡No puedes comprar!")
+    console.log("Edad del usuario: ", persona.edad)
+}if (persona.edad >= 18) {
+    console.log("Bienvenido!")
+    console.log("Edad del usuario: ", persona.edad)
+} else {
+    console.log("No ingreso ninguna edad")
+}
+
+
+
+// ["remera", "shorts", "camisa", "pantalon", "billetera", "lentes"]
+alert("Productos disponibles: remera, shorts, camisa, pantalon, billetera, lentes")
+let productos = ["remera", "shorts", "camisa", "pantalon", "billetera", "lentes"]
+let productoSolicitado = prompt("Ingresar un producto que esté disponible")
+let cantidadComprada
 let total = 0
 
 function cantidad(cantidad, precio) {
     return cantidad * precio
 }
 
-while (seleccionarProductos != 0, seleccionarProductos != "ESC" ){
-    switch (seleccionarProductos) {
-        case 1:
-            cantidadComprada = prompt("Indicar cantidad de remeras")
-            total += cantidad(cantidadComprada, 4500)
-            break;
-        case 2:
-            cantidadComprada = prompt("Indicar cantidad de shorts")
-            total += cantidad(cantidadComprada, 2500)
-            break;
-        case 3:
-            cantidadComprada = prompt("Indicar cantidad de camisas")
-            total += cantidad(cantidadComprada, 12000)
-            break;
-        case 4:
-            cantidadComprada = prompt("Indicar cantidad de pantalones")
-            total += cantidad(cantidadComprada, 10000)
-            break;
-        case 5:
-            cantidadComprada = prompt("Indicar cantidad de billeteras")
-            total += cantidad(cantidadComprada, 4500)
-            break;
-        case 6:
-            cantidadComprada = prompt("Indicar cantidad de lentes")
-            total += cantidad(cantidadComprada, 7000)
-            alert("la compra es de un total de: " + total)
-            break;
-
-        default:
-            break;
+while (true) {
+    
+    if (productos.includes(productoSolicitado)) {
+        switch (productoSolicitado) {
+            case "remera":
+                cantidadComprada = prompt("Indicar cantidad de remeras");
+                total += cantidad(cantidadComprada, 4500);
+                break;
+            case "shorts":
+                cantidadComprada = prompt("Indicar cantidad de shorts");
+                total += cantidad(cantidadComprada, 2500);
+                break;
+            case "camisa":
+                cantidadComprada = prompt("Indicar cantidad de camisas");
+                total += cantidad(cantidadComprada, 12000);
+                break;
+            case "pantalon":
+                cantidadComprada = prompt("Indicar cantidad de pantalones");
+                total += cantidad(cantidadComprada, 10000);
+                break;
+            case "billetera":
+                cantidadComprada = prompt("Indicar cantidad de billeteras");
+                total += cantidad(cantidadComprada, 4500);
+                break;
+            case "lentes":
+                cantidadComprada = prompt("Indicar cantidad de lentes");
+                total += cantidad(cantidadComprada, 7000);
+                break;
+            default:
+                break;
+        }
+        alert("La compra es de un total de: " + total);
+        break; 
+    } else {
+        alert("El producto solicitado no está disponible. Por favor, ingrese un producto válido.");
+    }
 }
-alert("la compra es de un total de: " + total)
-break
-}
-
-
-edad = Number(prompt("Confirme su edad: "))
-
-if (edad < 18){
-    alert ("Eres menor de edad, ¡No puedes comprar!")
-    console.log("Edad del usuario: ", edad)
-}if (edad >= 18) {
-    alert ("Bienvenido!")
-    console.log("Edad del usuario: ", edad)
-} else {
-    alert("Chau")
-    console.log("No ingreso ninguna edad")
-}
-
 
